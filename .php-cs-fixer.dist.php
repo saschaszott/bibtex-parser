9520a83653e751171569a391dd3608ae11dd8f3a
Sascha Szott <szott@gmx.de>
2021-07-21 23:57:35 +0200

    fixed trailing_comma_in_multiline elements option

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -27,7 +27,7 @@ return (new PhpCsFixer\Config())
             'elements' => ['method', 'property']
         ],
         'trailing_comma_in_multiline' => [
-            'elements' => ['arrays']
+            'elements' => ['arrays', 'arguments', 'parameters']
         ],
     ])
     ->setFinder(
