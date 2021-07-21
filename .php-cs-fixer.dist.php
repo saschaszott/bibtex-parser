2656c96d4a192360d67f7a5cf4de06be08ba3413
Sascha Szott <szott@gmx.de>
2021-07-21 23:35:31 +0200

    fixed breaking API change

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,7 +9,7 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 EOF;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setCacheFile(__DIR__.'/var/.php_cs.cache')
     ->setRiskyAllowed(true)
     ->setRules([
